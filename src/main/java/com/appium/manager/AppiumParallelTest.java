d5da918d023cc643119a672216b5af38024a30f3
Bruce Tu <bruce@mediabrix.com>
2017-04-20 14:13:27 -0400

    Added updated wda bundleid cap

--- src/main/java/com/appium/manager/AppiumParallelTest.java
+++ src/main/java/com/appium/manager/AppiumParallelTest.java
@@ -656,6 +656,7 @@ public class AppiumParallelTest extends TestListenerAdapter implements ITestList
                         iosDevice.getDeviceName(device_udid));
         iOSCapabilities.setCapability(MobileCapabilityType.UDID, device_udid);
         iOSCapabilities.setCapability(IOSMobileCapabilityType.XCODE_CONFIG_FILE, prop.getProperty("XCODE_CONFIG_FILE"));
+        iOSCapabilities.setCapability("updatedWDABundleId", prop.getProperty("UPDATED_WDA_BUNDLEID"));
         return iOSCapabilities;
     }
 
